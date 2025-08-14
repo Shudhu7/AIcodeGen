3a731a7b0780d241f908d1826b5bc0b957c1e96d
Shudhu7 <mr.shudhuingle@gmail.com>
2025-08-14 20:38:12 +0530

    Initial commit

---
+++ src/test/java/com/aicodegen/backend/AiCodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aicodegen.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
